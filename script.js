dde3f6dd87abb9600de84e2113361ef71a0345b3
hemme1m <markhemmert@gmail.com>
2017-08-08 09:39:11 -0400

    removed unnecessary variables from scope, adjusted random selector equation to be less convoluted

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 var app = angular.module('randomApp', []);
 
 app.controller('wordController', function($scope) {
-	$scope.wordPool = ["mommy", "daddy", "potty", "toys", "candy", "cake", "ice cream", "nana boo boo", "ouch", "wahhh", "woops", "wowza"];
+	var wordPool = ["mommy", "daddy", "potty", "toys", "candy", "cake", "ice cream", "nana boo boo", "ouch", "wahhh", "woops", "wowza"];
 	
-	$scope.phrasePool = [
+	var phrasePool = [
 	"wubba lubba dub dub",
 	"rubber baby buggy bumpers",
 	"i want ice cream",
@@ -17,32 +17,32 @@ app.controller('wordController', function($scope) {
 	"move it football head"
 	];
 
-	$scope.styles = ["red", "blue", "green", "purple", "gold", "brown", "orange", "dotted", "solid", "bold", "underline"];
+	var styles = ["red", "blue", "green", "purple", "gold", "brown", "orange", "dotted", "solid", "bold", "underline"];
 
 	$scope.words = [];
 
 	$scope.addWord = function() {
-		var randomWord = $scope.wordPool[Math.floor(Math.random() * (10-0 + 1) + 0)];
-		var randomWordObj = {name: randomWord, style: "nostyle"}
+		var randomWord = wordPool[Math.floor(Math.random() * wordPool.length)];
+		var randomWordObj = {name: randomWord};
 		$scope.words.push(randomWordObj);
 	};
 
 	$scope.addPrettyWord = function() {
-		var randomWord = $scope.wordPool[Math.floor(Math.random() * (10-0 + 1) + 0)];
-		var randomStyle = $scope.styles[Math.floor(Math.random() * (10-0 + 1) + 0)];
+		var randomWord = wordPool[Math.floor(Math.random() * wordPool.length)];
+		var randomStyle = styles[Math.floor(Math.random() * styles.length)];
 		var randomWordObj = {name: randomWord, style: randomStyle};
 		$scope.words.push(randomWordObj);
 	};
 
 	$scope.addPhrase = function () {
-		var randomPhrase = $scope.phrasePool[Math.floor(Math.random() * (10-0 + 1) + 0)];
-		var randomPhraseObj = {name: randomPhrase, style: "nostyle"}
+		var randomPhrase = phrasePool[Math.floor(Math.random() * phrasePool.length)];
+		var randomPhraseObj = {name: randomPhrase};
 		$scope.words.push(randomPhraseObj);
 	};
 
 	$scope.addPrettyPhrase = function() {
-		var randomPhrase = $scope.phrasePool[Math.floor(Math.random() * (10-0 + 1) + 0)];
-		var randomStyle = $scope.styles[Math.floor(Math.random() * (10-0 + 1) + 0)];
+		var randomPhrase = phrasePool[Math.floor(Math.random() * phrasePool.length)];
+		var randomStyle = styles[Math.floor(Math.random() * styles.length)];
 		var randomPhraseObj = {name: randomPhrase, style: randomStyle};
 		$scope.words.push(randomPhraseObj);
 	};
